49a73b802d24f8edea1818919ff990ca761c0241
DELL <DELL@DESKTOP-IOF8J2J>
2025-01-15 17:07:47 +0530

    Test

---
+++ BusinessLayer/Services/Services/BorrowService.cs
@@ -0,0 +1,34 @@
+﻿using BusinessLayer.DTOs;
+using BusinessLayer.Services.Interface;
+using DataAccessLayer.Entities;
+
+namespace BusinessLayer.Services.Services
+{
+    public class BorrowService : IBorrowService
+    {
+        public Task<ServiceResponse> AddBorrow(Borrow borrow)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse?> DeleteBorrow(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<List<Borrow>> GetAllBorrows()
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Borrow> GetBorrow(int? id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<ServiceResponse> UpdateBorrow(int? id, Borrow borrow)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
